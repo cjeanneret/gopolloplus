1715bc6009b4160398ae16a4734ff3e9ed2aae7e
Cédric Jeanneret <cjeanneret@internux.ch>
2021-03-06 17:14:12 +0100

    Get rid of Podman, Grafana, InfluxDB
    This new code change, while huge, makes things easier to manage. It
    reduces the footprint, dropping dependencies, especially for the graph
    part.
    
    It now uses plain fyne canvas in order to show current, average and max
    values for split time, power and strokes per minutes.
    
    From now on, the history consists in CSV files. This allows to load them
    in any sheet software (Excel, LibreCalc, even GoogleSheet) and get nice
    graphs out of the data.
    
    Please note there's an additional field, "Timestamp", holding the time
    of the stroke - it's different than the TotalTime, since this one shows
    the exercice duration.

--- pkg/apolloUtils/apolloUtils.go
+++ pkg/apolloUtils/apolloUtils.go
@@ -1,69 +1,77 @@
 package apolloUtils
 import (
+  "log"
   "os"
-  "strconv"
+  "path"
+  "time"
   "gopkg.in/ini.v1"
+  homedir "github.com/mitchellh/go-homedir"
 )
-
-func Check_for_socket(socket string) bool {
-  _, err := os.Stat(socket)
-  return !os.IsNotExist(err)
-}
-
-func Parse_apollo(data string) *ApolloData {
-  totalMinutes, _ := strconv.ParseInt(data[3:5], 10, 64)
-  totalSeconds, _ := strconv.ParseInt(data[5:7], 10, 64)
-  totalTime := totalMinutes*60+totalSeconds
-  distance, _ := strconv.ParseInt(data[7:12], 10, 64)
-  MinutesTo500m, _ := strconv.ParseInt(data[13:15], 10, 64)
-  SecondsTo500m, _ := strconv.ParseInt(data[15:17], 10, 64)
-  timeTo500m := MinutesTo500m*60+SecondsTo500m
-  spm, _ := strconv.ParseInt(data[17:20], 10, 64)
-  watt, _ := strconv.ParseInt(data[20:23], 10, 64)
-  calph, _ := strconv.ParseInt(data[23:27], 10, 64)
-  level, _ := strconv.ParseInt(data[27:29], 10, 64)
-
-  output := ApolloData{
-    TotalTime: totalTime,
-    Distance: distance,
-    TimeTo500m: timeTo500m,
-    SPM: spm,
-    Watt: watt,
-    CalPerH: calph,
-    Level: level,
-  }
-
-  return &output
-}
 
 func LoadConfig(config_file string) *ApolloConfig {
   cfg, err := ini.Load(config_file)
   if err != nil {
     return nil
   }
-  manage_pod, err := cfg.Section("gopolloplus").Key("manage_pod").Bool()
+  fullscreen, err := cfg.Section("gopolloplus").Key("fullscreen").Bool()
   if err != nil {
-    manage_pod = false
+    fullscreen = false
+  }
+
+  history, err := homedir.Expand(cfg.Section("gopolloplus").Key("history_dir").String())
+  if err != nil {
+    history = "/tmp"
   }
 
   config := ApolloConfig{
-    Pod: manage_pod,
-    PodName: cfg.Section("gopolloplus").Key("pod_name").String(),
-    PodSocket: cfg.Section("gopolloplus").Key("podman_socket").String(),
     Socket: cfg.Section("gopolloplus").Key("socket").String(),
     Logfile: cfg.Section("gopolloplus").Key("log_file").String(),
-    Grafana_img: cfg.Section("grafana").Key("image").String(),
-    Grafana_data: cfg.Section("grafana").Key("data").String(),
-    Influx_host: cfg.Section("influxdb").Key("host").String(),
-    Influx_adm: cfg.Section("influxdb").Key("admin_user").String(),
-    Influx_adm_pwd: cfg.Section("influxdb").Key("admin_password").String(),
-    Influx_user: cfg.Section("influxdb").Key("user").String(),
-    Influx_pwd: cfg.Section("influxdb").Key("password").String(),
-    Influx_db: cfg.Section("influxdb").Key("database").String(),
-    Influx_img: cfg.Section("influxdb").Key("image").String(),
-    Influx_data: cfg.Section("influxdb").Key("data").String(),
-    Influx_conf: cfg.Section("influxdb").Key("config").String(),
+    FullScreen: fullscreen,
+    HistoryDir: history,
   }
 
   return &config
+}
+
+func FindMinMax(a []uint64) (min, max uint64) {
+  min = a[0]
+  max = a[0]
+  for _, value := range a {
+    if value < min {
+      min = value
+    }
+    if value > max {
+      max = value
+    }
+  }
+  return
+}
+
+func Average(a []uint64) (avg float64) {
+  total := 0.0
+  for _, value := range a{
+    total += float64(value)
+  }
+  avg = total / float64(len(a))
+  return
+}
+
+func GetHistoryFile(cfg *ApolloConfig) (p string) {
+  log.Printf("Checking for %s", cfg.HistoryDir)
+  _, err := os.Stat(cfg.HistoryDir)
+  if os.IsNotExist(err) {
+    err := os.Mkdir(cfg.HistoryDir, 0755)
+    if err != nil {
+      log.Fatal(err)
+    }
+  }
+
+  t := time.Now()
+  p = path.Join(cfg.HistoryDir, t.Format("2006-01-02-150405"))
+  return
+}
+
+func CSVHeader(f *os.File) {
+  header := []byte("timestamp,totalTime,distance,timeTo500m,SPM,watt,calPerH,level\n")
+  f.Write(header)
 }
